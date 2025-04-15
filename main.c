c31624a4d8a540a2e6acdc6021349a4b6a0c8933
cacatuacommander <144157854+cacatuacommander@users.noreply.github.com>
2025-04-15 03:10:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -48,6 +48,26 @@ int	main(int argc, char **argv, char **envp)
 		if (!s)
 			break; // Uscita se l'utente digita EOF (Ctrl+D)
 		add_history(s);
+
+	/* //s = "\"\" ahaha \" ohohoh\'ehe\'\"\"  iii\"\"\"ahah\'\"ww\'\'\' |   ";
+		s = get_next_line(0);
+		arr = create_arr(s);
+		//da qui vanno nel fork i sa e s va liberata fuori dal fork
+		tokens = separete_in_tokens(s, arr);
+		free(arr);
+		free (s);
+		check_ambig_redirect(&tokens, env);
+		expand_dollar(&tokens, env);
+		//printf("(%s)\n", s);
+		// for(int i = 0; i < ft_strlen(s) - 1; i++)
+		//	printf("%d", arr[i]);
+		printf("\n");
+		ft_lstiter_tok(tokens , prnt);
+		ft_lstclear_tok(&tokens);
+	//	free(arr);
+	//	free(s); */
+
+		
 		handle_pipes_or_execute(s, &env);
 		args = ft_split(s, ' ');
 		if (ft_strncmp(args[0], "export", 6) == 0)
