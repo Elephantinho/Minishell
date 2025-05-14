ffc33023773a2cf69b43fa1c4631185b82809ab2
Mohamed Shahein <mohamedpato143@gmail.com>
2025-05-14 20:55:40 +0200

    Add files via upload

---
+++ shahein/parsing/create_arr_of_lists.c
@@ -0,0 +1,84 @@
+#include "../includes/minishell.h"
+
+void	arr_of_lists_free(char *s, char **env)
+{
+	free_env(env);
+	if (s)
+		free(s);
+	write(2, "malloc failed\n", 14);
+	exit(1);
+}
+
+int	count_n_of_lists(t_token *token)
+{
+	int	len;
+	int	flag;
+
+	len = 0;
+	flag = 1;
+	while (token)
+	{
+		if (token->e_tk_type == NORMAL && flag == 1)
+		{
+			len++;
+			flag = 0;
+		}
+		else if (token->e_tk_type == PIPE)
+			flag = 1;
+		token = token->next;
+	}
+	return (len);
+}
+
+void	arr_of_lists_h(t_token **tokens, t_token **first, \
+							t_token ***arr_of_lists, int *i)
+{
+	t_token	*prev;
+
+	while (*tokens && (*tokens)->e_tk_type != PIPE)
+	{
+		prev = (*tokens);
+		*tokens = (*tokens)->next;
+	}
+	if (*tokens && (*tokens)->e_tk_type == PIPE)
+	{
+		(*arr_of_lists)[*i] = (*first);
+		prev->next = NULL;
+		prev = (*tokens);
+		(*tokens) = (*tokens)->next;
+		free(prev->token);//credo ci vadano sti 2 free
+		free(prev);
+		if ((*tokens))//if forse non serve(mafunziona) ((*first) = (*tokens); serve pero solo l'if forse e' inutile)
+			(*first) = (*tokens);
+	}
+	else if (!(*tokens))
+	{
+		(*arr_of_lists)[*i] = *first;
+		prev->next = NULL;
+	}
+	(*i)++;
+}
+
+t_token	**create_arr_of_lists(char *s, char **env, t_token **tokens)
+{
+	t_token	**arr_of_lists;
+	int		i;
+	int		len;
+	t_token	*first;
+
+	len = count_n_of_lists(*tokens);
+	i = 0;
+	first = *tokens;
+	arr_of_lists = malloc(sizeof(t_token *) * (len + 1));
+	if (!arr_of_lists)
+	{
+		ft_lstclear_tok(tokens);
+		arr_of_lists_free(s, env); //da ricontrollare
+	}
+	while (i < len && tokens && *tokens)
+	{
+		arr_of_lists_h(tokens, &first, &arr_of_lists, &i);
+	}
+	arr_of_lists[i] = NULL;
+	return (arr_of_lists);
+}
---
+++ shahein/parsing_forks.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_forks.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshahein <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 21:22:59 by ale               #+#    #+#             */
+/*   Updated: 2025/05/10 17:09:06 by mshahein         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+void	second_fork_failed(char *s, int *arr, char **env, int exit_code)
+{
+	free(arr);
+	free (s);
+	free_env(env);
+//	printf("error_code1111: %d\n", error_code);
+	exit (exit_code); //da modificare error code
+}
+
+void	second_fork_worked(char *s, int *arr, char **env, t_token **tokens)
+{
+//	char	***arr_of_matrx;
+
+	*tokens = separete_in_tokens(s, arr);
+//	ft_lstiter_tok(*tokens , prnt);
+	free(arr);
+	free (s);
+	check_ambig_redirect(tokens, env);
+	expand_dollar(tokens, env);
+//	printf("\n");
+//	arr_of_matrx =  create_array_of_matrix(NULL, env, *tokens);//da ;evare
+//	ft_lstiter_tok(*tokens , prnt);
+	ft_lstclear_tok_mod(tokens);
+//	if (arr_of_matrx)
+//		free_cmnds(arr_of_matrx); //da levare
+	free_env(env);
+	exit (0);
+}
+
+t_token	**first_fork_worked(char *s, char **env, t_token **tokens)
+{
+//	char	***arr_of_matrx;
+	int		*arr;
+
+	t_token	**arr_of_lists;
+
+	arr = create_arr(s, env);
+	*tokens = separete_in_tokens(s, arr);
+	free(arr);
+	check_ambig_redirect(tokens, env);
+	expand_dollar(tokens, env);
+	printf("\n");//da levare
+	ft_lstiter_tok(*tokens , prnt);//sarà da levare stampa solo la lista dei token
+
+	//arr_of_matrx = create_array_of_matrix(s, env, *tokens);//da non fare piu e fare dentro a ogni pipe la sua matrice
+	arr_of_lists = create_arr_of_lists(s, env, tokens);
+
+	int i = 0;
+	while(arr_of_lists[i] != NULL)
+	{
+		printf("lista %d\n", i);
+		ft_lstiter_tok(arr_of_lists[i], prnt);
+		i++;
+	}
+	//da levare questo doppio ciclo for che stampa la matrice
+/* 	for (int z = 0; z < 2; z++)
+	{
+		for (int y = 0; y < 3; y++)
+			printf("s[%d][%d]: %s \n", z, y, arr_of_matrx[z][y]);
+	} */
+	ft_lstclear_tok_mod(tokens);//non va piu bene perche la lista
+	//non va piu liberata ora che si fa l?arr_of lists
+	//e andra ritornato larray of list non arr_of matrx che non va piu fatto
+	return (arr_of_lists); //da cambiare ritornando arr_of_lists
+}
+
+void	second_fork_parsing(char *s, int *arr, char **env, t_token **tokens)
+{
+	free_env(env);
+	*tokens = separete_in_tokens(s, arr);
+	free(arr);
+	free (s);
+	ft_lstclear_tok(tokens);
+	exit(0);
+}
+
+void	first_fork_parsing(char *s, char **env, t_token **tokens)
+{
+	int	*arr;
+	int	pid2;
+	int	exit_code_pars;
+
+	arr = create_arr(s, env);
+	pid2 = fork();
+	if (pid2 == -1)
+		perror("fork failed");//non so se va bene come errore
+	if (pid2 == 0)
+		second_fork_parsing(s, arr, env, tokens);
+	else
+	{
+		waitpid(pid2, &exit_code_pars, 0);
+	//	printf("error_codesecfork: %d\n", exit_code_pars);
+		exit_code_pars = transl_exit_code(exit_code_pars);
+		if (exit_code_pars != 0)
+			second_fork_failed(s, arr, env, exit_code_pars);
+		else
+			second_fork_worked(s, arr, env, tokens);
+	}
+}
