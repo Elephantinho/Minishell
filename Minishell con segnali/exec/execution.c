9f36d45dca183a96cd4a7febb6450e3f70ec3d08
cacatuacommander <144157854+cacatuacommander@users.noreply.github.com>
2025-05-19 23:28:30 +0200

    Add files via upload

---
+++ Minishell con segnali/exec/execution.c
@@ -0,0 +1,357 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-ross <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 21:45:16 by mshahein          #+#    #+#             */
+/*   Updated: 2025/05/19 23:12:14 by ade-ross         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	print_matrix(char **matrix)
+{
+	int	i = 0;
+
+	if (!matrix)
+	{
+		printf("Matrix is NULL\n");
+		return;
+	}
+
+	while (matrix[i])
+	{
+		printf("matrix[%d] = \"%s\"\n", i, matrix[i]);
+		i++;
+	}
+}
+
+void	execute_mod(char **cmds, char ***env, int *exit_code, int save_fd_in)
+{
+	char	*path;
+
+	if (!cmds)
+		return ;
+	path = find_path(cmds[0], env, exit_code);
+	if (!path)
+	{
+		return ;
+	}
+	close(save_fd_in);
+	execve(path, cmds, *env);
+	perror("Minishell: Execve failed in execute");
+	free(path);
+	free_cmnds(&cmds);
+}
+
+char	**built_in_or_execute(char ***env, t_token **tokens, int *exit_code, int original_fd_in, int original_fd_out, int heredoc_alredy_done)
+{
+	char	**cmnds;
+	int		save_fd_in;
+//	int		save_fd_out;
+//	int		heredoc_alredy_done;
+
+	save_fd_in = dup(STDIN_FILENO);
+//	save_fd_out = dup(STDOUT_FILENO);
+//	printf("curr: %s\n", (*tokens)->token);
+	//heredoc_alredy_done = handle_heredocs(tokens, save_fd_in, original_fd_in, original_fd_out);
+	close(original_fd_in);//credo sti 2 close vadano qua ma non sono sicuro
+	close(original_fd_out);
+	handle_redirections(tokens, heredoc_alredy_done);
+	cmnds = create_matrix_and_free_arr_of_lists(tokens, tokens, &cmnds);
+	//printf("hooo\n");
+	print_matrix(cmnds);
+	if (cmnds && cmnds[0])
+	{
+			if ((ft_strncmp(cmnds[0], "export", 6) == 0) && cmnds[0][6] == '\0')
+				ft_export(cmnds, env);
+			else if ((ft_strncmp(cmnds[0], "cd", 2) == 0) && cmnds[0][2] == '\0')
+				ft_cd_pars(cmnds, env);
+			else if ((ft_strncmp(cmnds[0], "pwd", 3) == 0) && cmnds[0][3] == '\0')
+				ft_pwd();
+			else if ((ft_strncmp(cmnds[0], "env", 3) == 0) && cmnds[0][3] == '\0')
+				ft_env(*env);
+			else if ((ft_strncmp(cmnds[0], "unset", 5) == 0) && cmnds[0][5] == '\0')
+				ft_unset(cmnds, env);
+			else if ((ft_strncmp(cmnds[0], "echo", 4) == 0) && cmnds[0][4] == '\0')
+				ft_echo(&(cmnds[1]));
+			else if ((ft_strncmp(cmnds[0], "exit", 4) == 0) && cmnds[0][4] == '\0')
+				ft_exit(&cmnds, NULL, *env, exit_code);//non so se c'e ancora s da liberare
+			else
+				execute_mod(cmnds, env, exit_code, save_fd_in);
+	//		printf("hi\n");
+			//print_matrix(cmnds);
+	}
+	free_cmnds(&cmnds);//credo ma non sono sicuro ci vada
+	cmnds = NULL;//forse non serve
+	//dup2(save_fd_in, STDIN_FILENO);
+	//close(save_fd_in);//giusto qua? va anche dentro execute mod forse
+//	dup2(save_fd_out, STDOUT_FILENO);
+	//printf("quii\n");
+	return (cmnds);
+}
+
+
+void	ft_execution(t_token **arr_of_lists, char ***env, int *status)
+{
+	int		i = 0;
+	int		count = 0;
+	int		cpipe[2];
+	int		prevpipe = -1;
+//	int		fd_in = -1;
+	//int		fd_out = -1;
+	pid_t	pids[256];
+ 	int		save_fd_in;
+	save_fd_in = dup(STDIN_FILENO);
+	int	save_fd_out;
+	save_fd_out = dup(STDOUT_FILENO);/* */
+	//int		heredoc_alredy_done;
+	t_token **first = arr_of_lists;
+	t_token ***first2 = &arr_of_lists;
+	int	heredoc_alredy_done;
+	int	heredoc_pipes[2][256];
+
+	int fd;
+	while(count < 256)
+	{
+		heredoc_pipes[0][count] = 0;
+		heredoc_pipes[1][count] = 0;
+		count++;
+	}
+	count = 0;
+	while(arr_of_lists[count])
+	{
+		heredoc_pipes[1][count] = handle_heredocs(&(arr_of_lists[count]), &fd, *env);
+		heredoc_pipes[0][count] = fd;
+		write(2, "exitst: \n", 8);
+		char *srr = ft_itoa(g_exit_status);
+		write(2, srr, ft_strlen(srr));
+		write(2, "\n", 1);
+		if (g_exit_status == 130)
+		{
+			int l = 0;
+			while(l < 256)//forse non sta funzionando perfettamente perche c'era ancora un/qualche fd aperti
+			{
+				if (heredoc_pipes[1][l] == 1)
+				{
+				//	printf("process : %d, waaaaaaaaaaaaaaaaaa\n", count);
+						close(heredoc_pipes[0][l]);
+				}
+				l++;
+			}
+			t_token *temp;
+			t_token *temp2;
+			l = 0;
+			while((arr_of_lists)[l])
+			{
+				temp = (arr_of_lists)[l];
+				while(temp)
+				{
+					temp2 = temp->next;
+					if((temp)->token)
+						free((temp)->token);
+					free(temp);
+					temp = temp2;
+				}
+				l++;
+			}
+			return ;
+		}
+		count++;
+	}
+
+	count = 0;
+	while (arr_of_lists[count] && g_exit_status == 0) // Ciclo per eseguire ogni comando
+	{
+			heredoc_alredy_done = 0;
+			int save_fd = dup(STDIN_FILENO);
+			if (heredoc_pipes[1][count] == 1)
+			{
+				if (prevpipe != -1)
+					close(prevpipe);
+				prevpipe = heredoc_pipes[0][count];
+				heredoc_alredy_done = 1;
+			}
+			// Crea una pipe se non è l'ultimo comando
+			if (arr_of_lists[count + 1])
+			{
+				if (pipe(cpipe) == -1)
+				{
+					perror("Minishell: pipe");
+					exit(1);// non v bene exit qua
+				}
+			}
+
+			pids[i] = fork(); // Crea il processo figlio
+			if (pids[i] == -1)
+			{
+				perror("Minishell: fork");
+				exit(1);// non va bene exit qua
+			}
+			if (pids[i] == 0)
+			{
+				signal(SIGINT, SIG_DFL);
+				signal(SIGQUIT, SIG_DFL);
+				int z = 0;
+				while(z < 256)//forse non sta funzionando perfettamente perche c'era ancora un/qualche fd aperti
+				{
+					if (heredoc_pipes[1][z] == 1)
+					{
+					//	printf("process : %d, waaaaaaaaaaaaaaaaaa\n", count);
+						if(heredoc_pipes[0][z] != prevpipe)
+							close(heredoc_pipes[0][z]);
+					}
+					z++;
+				}
+				if (prevpipe != -1 || heredoc_alredy_done == 1)
+				{
+					dup2(prevpipe, STDIN_FILENO);
+					close(prevpipe);
+				}
+				// Se non è l'ultimo comando, collega stdout alla pipe attuale
+				if (arr_of_lists[count + 1])
+				{
+					close(cpipe[0]);
+					dup2(cpipe[1], STDOUT_FILENO);
+					close(cpipe[1]);
+				}
+				// Esegui il comando
+			/* int	heredoc_alredy_done =  handle_heredocs(arr_of_lists, save_fd_in);*/
+			//	handle_redirections(arr_of_lists, heredoc_alredy_done);
+			//	printf("arr_of_lists[%d]->token: %s\n", count, (arr_of_lists[count])->token);
+
+				int j;
+				t_token *temp;
+				t_token *temp2;
+
+
+				j = 0;
+				//printf("(*first2)[j]: %s\n", (*first2)[j]->token);
+				while(j < count)
+				{
+					temp = (arr_of_lists)[j];
+					while(temp)
+					{
+						temp2 = temp->next;
+						if((temp)->token)
+							free((temp)->token);
+						free(temp);
+						temp = temp2;
+					}
+					j++;
+				}
+
+				if (heredoc_alredy_done == 1)
+				{
+					t_token *prev = NULL;
+					t_token *curr = arr_of_lists[count];
+					//temp = &(arr_of_lists[count]);
+					while(curr)
+					{
+						if (curr->e_tk_type == HEREDOC && curr->next && curr->next->e_tk_type == NORMAL)
+						{
+							remove_redir_tokens(&(arr_of_lists[count]), prev, curr);
+							if (prev == NULL)
+								curr = arr_of_lists[count];
+							else
+								curr = prev->next;
+						}
+						else
+						{
+							prev = curr;
+							curr = curr->next;
+						}
+					}
+				}
+				j = count + 1;
+				while((arr_of_lists)[j])
+				{
+					temp = (arr_of_lists)[j];
+					while(temp)
+					{
+						temp2 = temp->next;
+						if((temp)->token)
+							free((temp)->token);
+						free(temp);
+						temp = temp2;
+					}
+					j++;
+				}
+
+				built_in_or_execute(env, &arr_of_lists[count], status, save_fd_in, save_fd_out, heredoc_alredy_done);
+				dup2(save_fd, STDIN_FILENO);
+				close(save_fd);
+				if (*env)//non so se serve sto if
+					free_env(*env);
+				//da sistemare qua dovrebbe liberare l array di liste ma non lo fa perché le liste sono arrivate alla fine (credo)
+				/* int j;
+				j = 0;
+				t_token *temp;
+				t_token *temp2;
+				//printf("(*first2)[j]: %s\n", (*first2)[j]->token);
+				while((*first2)[j])
+				{
+					printf("primo while\n");
+					temp = (*first2)[j];
+					while(temp)
+					{
+						printf("secondo while\n");
+						temp2 = temp->next;
+						if((temp)->token)
+							free((temp)->token);
+						free(temp);
+						temp = temp2;
+					}
+					j++;
+				} */
+				free(*first2);
+				exit(*status);
+			}
+			else  // Codice eseguito dal processo padre
+			{
+				// Chiudi la pipe precedente se non è più necessaria
+				if (prevpipe != -1)
+					close(prevpipe);
+
+				// Se ci sono più comandi, salva la pipe per il prossimo comando
+				if (arr_of_lists[count + 1])
+				{
+					prevpipe = cpipe[0];
+					close(cpipe[1]);
+				}
+				if (heredoc_alredy_done == 1)
+					dup2(save_fd_in, STDIN_FILENO);
+			}
+			i++;
+		//}
+		count++;
+	}
+	int j;
+	j = 0;
+	t_token *temp;
+	t_token *temp2;
+	//printf("first[j]: %s", first[j]->token);
+	while(first[j])
+	{
+		temp = first[j];
+		while(temp)
+		{
+			temp2 = temp->next;
+			free((temp)->token);
+			free(temp);
+			temp = temp2;
+		}
+		j++;
+	}
+	free(first);
+	close (save_fd_in);//questi 2 close penso vadano qui ma non sono sicuro al 100% vanno anche chiusi nei figli mi sa
+	close (save_fd_out);
+	// Aspetta la fine di tutti i figli
+	for (j = 0; j < i; j++)
+		waitpid(pids[j], status, 0);
+}
+
+
---
+++ Minishell con segnali/parsing/dollar_special_cases.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dollar_special_cases.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 18:55:23 by ale               #+#    #+#             */
+/*   Updated: 2025/04/23 01:09:02 by ale              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	get_last_exit_code(char **env)
+{
+	int		exit_code;
+
+	if (ft_getenv("last_ex_cd", env))
+	{
+		exit_code = ft_atoi(ft_getenv("last_ex_cd", env));
+	}
+	else
+		exit_code = 0;//se cancellano la variabile ritorno 0(?)
+	if (WIFEXITED(exit_code))
+	{
+		// Child terminated normally
+		exit_code = WEXITSTATUS(exit_code);
+		return (exit_code); // You can propagate the same exit code
+	}
+	else if (WIFSIGNALED(exit_code))
+	{
+		// Child terminated by a signal
+		exit_code = WTERMSIG(exit_code);
+		return (128 + exit_code);
+	}
+	else if (WIFSTOPPED(exit_code))
+	{
+		// Child was stopped (not terminated)
+		exit_code = WSTOPSIG(exit_code);
+		return (128 + exit_code); // Similar convention
+	}
+	else if (WIFCONTINUED(exit_code))
+	{
+		return (0); // No error
+	}
+	else
+	{
+		// This should rarely happen
+		return (1); // Generic error
+	}
+}
+
+void	double_dollar_case(t_str *st, char **env)
+{
+	st->env_content = ft_itoa(getpid());
+	if (!(st->env_content))
+	{
+		write(2, "malloc failed\n", 14);
+		free_env(env);//da levare credo
+		ft_lstclear_tok(&(st->first));
+		exit(1);//exit cosi va bene?
+	}
+	(st->i)++;
+//		printf("env_cont1: %s\n", *env_content);
+}
+
+void	dollar_qmark_case(t_str *st, char **env)
+{
+	st->env_content = ft_itoa(get_last_exit_code(env));
+	if (!(st->env_content))
+	{
+		write(2, "malloc failed\n", 14);
+		free_env(env);//da levare credo
+		ft_lstclear_tok(&(st->first));
+		exit(1);//exit cosi va bene?
+	}
+	(st->i)++;
+//	printf("env_cont2: %s\n", *env_content);
+}
+
+int	special_cases(t_str *st, char **env)
+{
+	if (st->s[st->i] == '$' && \
+		!(st->prev && st->prev->e_tk_type == HEREDOC))
+	{
+		double_dollar_case(st, env);
+	}
+	else if (st->s[st->i] == '?' && \
+		!(st->prev && st->prev->e_tk_type == HEREDOC))
+	{
+		dollar_qmark_case(st, env);
+	}
+	else if (st->s[st->i] != '"' && st->s[st->i] != '\'')
+	{
+		st->new_tok = ft_strdup(st->s);
+		if (!(st->new_tok))
+		{
+			write(2, "malloc failed\n", 14);
+			free_env(env);
+			ft_lstclear_tok(&(st->first));
+			exit(1);//exit cosi va bene?
+		}
+		return (0);
+	}
+	else // messo quest caso strano delle virgolette dopo il $ ma non so se lasciarlo
+		st->env_content = NULL;
+	return (1);
+}
